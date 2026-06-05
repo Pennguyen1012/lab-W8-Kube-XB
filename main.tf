008bc94624ea70999ae1c141b9f4546e43805e7a
Pen <pennguyen31@gmail.com>
2026-06-05 10:22:31 +0700

    added egress block to allow internet access to EC2

--- main.tf
+++ main.tf
@@ -67,6 +67,13 @@ resource "aws_security_group" "ec2_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # EC2 setup to run Kind K8s
